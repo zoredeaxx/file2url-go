d1cebbca5efd970c5cd5c3495eb3c62dfcf292cf
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-02-28 16:01:51 +0530

    Update downloader.go

--- bot/downloader/downloader.go
+++ bot/downloader/downloader.go
@@ -6,7 +6,7 @@ import (
 	"io"
 )
 
-const defaultPartSize = 64 * 1024
+const defaultPartSize = 512 * 1024
 
 // Download simply downloads a file from telegram
 func Download(ctx context.Context, client *tg.Client, location tg.InputFileLocationClass, output io.Writer, from, to int64) error {
